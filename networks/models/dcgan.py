2463113f77fbf382cb8b18835ebc383f4bc16f5d
Wesley Emeneker <1059409812@mil>
2018-08-15 16:45:53 -1000

    Fixing some basic PEP8 stuff. Getting familiar with code.
    End goal is multiGPU support via batch-splitting for data parallelization.

--- networks/models/dcgan.py
+++ networks/models/dcgan.py
@@ -1,9 +1,10 @@
 import tensorflow as tf
 from .ops import linear, conv2d, conv2d_transpose, lrelu
 
-class dcgan(object):
-    def __init__(self, output_size=64, batch_size=64, 
-                 nd_layers=4, ng_layers=4, df_dim=128, gf_dim=128, 
+
+class DCGAN(object):
+    def __init__(self, output_size=64, batch_size=64,
+                 nd_layers=4, ng_layers=4, df_dim=128, gf_dim=128,
                  c_dim=1, z_dim=100, flip_labels=0.01, data_format="NHWC",
                  gen_prior=tf.random_normal, transpose_b=False):
 
@@ -18,13 +19,12 @@ class dcgan(object):
         self.flip_labels = flip_labels
         self.data_format = data_format
         self.gen_prior = gen_prior
-        self.transpose_b = transpose_b # transpose weight matrix in linear layers for (possible) better performance when running on HSW/KNL
-        self.stride = 2 # this is fixed for this architecture
+        self.transpose_b = transpose_b  # transpose weight matrix in linear layers for (possible) better performance when running on HSW/KNL
+        self.stride = 2  # this is fixed for this architecture
 
         self._check_architecture_consistency()
 
-
-        self.batchnorm_kwargs = {'epsilon' : 1e-5, 'decay': 0.9, 
+        self.batchnorm_kwargs = {'epsilon': 1e-5, 'decay': 0.9,
                                  'updates_collections': None, 'scale': True,
                                  'fused': True, 'data_format': self.data_format}
 
@@ -37,10 +37,10 @@ class dcgan(object):
 
         self.z = self.gen_prior(shape=[self.batch_size, self.z_dim])
 
-        with tf.variable_scope("discriminator") as d_scope:
+        with tf.variable_scope("discriminator"):
             d_prob_real, d_logits_real = self.discriminator(self.images, is_training=True)
 
-        with tf.variable_scope("generator") as g_scope:
+        with tf.variable_scope("generator"):
             g_images = self.generator(self.z, is_training=True)
 
         with tf.variable_scope("discriminator") as d_scope:
@@ -63,7 +63,7 @@ class dcgan(object):
                                            tf.summary.scalar("loss/d", self.d_loss)])
 
         g_sum = [tf.summary.scalar("loss/g", self.g_loss)]
-        if self.data_format == "NHWC": # tf.summary.image is not implemented for NCHW
+        if self.data_format == "NHWC":  # tf.summary.image is not implemented for NCHW
             g_sum.append(tf.summary.image("G", g_images, max_outputs=4))
         self.g_summary = tf.summary.merge(g_sum)
 
@@ -71,7 +71,7 @@ class dcgan(object):
         self.d_vars = [var for var in t_vars if 'discriminator/' in var.name]
         self.g_vars = [var for var in t_vars if 'generator/' in var.name]
 
-        with tf.variable_scope("counters") as counters_scope:
+        with tf.variable_scope("counters"):
             self.epoch = tf.Variable(-1, name='epoch', trainable=False)
             self.increment_epoch = tf.assign(self.epoch, self.epoch+1)
             self.global_step = tf.train.get_or_create_global_step()
@@ -87,13 +87,13 @@ class dcgan(object):
 
         self.z = tf.placeholder(tf.float32, [None, self.z_dim], name='z')
 
-        with tf.variable_scope("discriminator") as d_scope:
-            self.D,_ = self.discriminator(self.images, is_training=False)
+        with tf.variable_scope("discriminator"):
+            self.D, _ = self.discriminator(self.images, is_training=False)
 
-        with tf.variable_scope("generator") as g_scope:
+        with tf.variable_scope("generator"):
             self.G = self.generator(self.z, is_training=False)
 
-        with tf.variable_scope("counters") as counters_scope:
+        with tf.variable_scope("counters"):
             self.epoch = tf.Variable(-1, name='epoch', trainable=False)
             self.increment_epoch = tf.assign(self.epoch, self.epoch+1)
             self.global_step = tf.train.get_or_create_global_step()
@@ -110,11 +110,9 @@ class dcgan(object):
 
         return tf.group(d_optim, g_optim, name="all_optims")
 
-                                                   
     def generator(self, z, is_training):
-
         map_size = self.output_size/int(2**self.ng_layers)
-        num_channels = self.gf_dim * int(2**(self.ng_layers -1))
+        num_channels = self.gf_dim * int(2**(self.ng_layers - 1))
 
         # h0 = relu(BN(reshape(FC(z))))
         z_ = linear(z, num_channels*map_size*map_size, 'h0_lin', transpose_b=self.transpose_b)
@@ -129,18 +127,17 @@ class dcgan(object):
             num_channels /= 2
             chain = conv2d_transpose(chain,
                                      self._tensor_data_format(self.batch_size, map_size, map_size, num_channels),
-                                     stride=self.stride, data_format=self.data_format, name='h%i_conv2d_T'%h)
-            chain = tf.contrib.layers.batch_norm(chain, is_training=is_training, scope='bn%i'%h, **self.batchnorm_kwargs)
+                                     stride=self.stride, data_format=self.data_format, name='h%i_conv2d_T' % h)
+            chain = tf.contrib.layers.batch_norm(chain, is_training=is_training, scope='bn%i' % h, **self.batchnorm_kwargs)
             chain = tf.nn.relu(chain)
 
         # h1 = conv2d_transpose(h0)
         map_size *= self.stride
         hn = conv2d_transpose(chain,
                               self._tensor_data_format(self.batch_size, map_size, map_size, self.c_dim),
-                              stride=self.stride, data_format=self.data_format, name='h%i_conv2d_T'%(self.ng_layers))
+                              stride=self.stride, data_format=self.data_format, name='h%i_conv2d_T' % self.ng_layers)
 
         return tf.nn.tanh(hn)
-
 
     def discriminator(self, image, is_training):
 
@@ -150,12 +147,15 @@ class dcgan(object):
         chain = h0
         for h in range(1, self.nd_layers):
             # h1 = lrelu(BN(conv2d(h0)))
-            chain = conv2d(chain, self.df_dim*(2**h), self.data_format, name='h%i_conv'%h)
-            chain = tf.contrib.layers.batch_norm(chain, is_training=is_training, scope='bn%i'%h, **self.batchnorm_kwargs)
+            chain = conv2d(chain, self.df_dim*(2**h), self.data_format, name='h%i_conv' % h)
+            chain = tf.contrib.layers.batch_norm(chain, is_training=is_training, scope='bn%i' % h, **self.batchnorm_kwargs)
             chain = lrelu(chain)
 
         # h1 = linear(reshape(h0))
-        hn = linear(tf.reshape(chain, [self.batch_size, -1]), 1, 'h%i_lin'%self.nd_layers, transpose_b=self.transpose_b)
+        hn = linear(tf.reshape(chain, [self.batch_size, -1]),
+                    1,
+                    'h%i_lin' % self.nd_layers,
+                    transpose_b=self.transpose_b)
 
         return tf.nn.sigmoid(hn), hn
 
